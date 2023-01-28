6dfcb9fd1a346eae5cfb2c57d63169ca7bfe12c7
Anton Gordeev <aigordeev@alfabank.ru>
2023-01-28 18:39:09 +0300

    Beginning

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMMFirstAppMobile"
+include(":androidApp")
+include(":shared")
