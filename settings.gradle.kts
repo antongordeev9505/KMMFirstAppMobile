ae48aaf24c4dfb974c10c3faab94a7649fdda49c
Anton Gordeev <aigordeev@alfabank.ru>
2023-01-28 19:10:59 +0300

    Added modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,13 @@ dependencyResolutionManagement {
 rootProject.name = "KMMFirstAppMobile"
 include(":androidApp")
 include(":shared")
+include(":common:auth:api")
+include(":common:auth:presentation")
+include(":common:auth:data")
+include(":common:tournaments:api")
+include(":common:tournaments:presentation")
+include(":common:tournaments:data")
+include(":common:games:api")
+include(":common:games:presentation")
+include(":common:games:data")
+include(":common:core")
